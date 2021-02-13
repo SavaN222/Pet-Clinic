fe67949fa81465450cafc51e29094e2f399e8248
SavaN22 <savanakarada22@gmail.com>
2021-02-13 12:40:02 +0100

    Establish connection with database

--- src/com/petclinic/pets/PetDbUtil.java
+++ src/com/petclinic/pets/PetDbUtil.java
@@ -1,5 +1,32 @@
 package com.petclinic.pets;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 public class PetDbUtil {
 
+	private DataSource dataSource;
+	private Connection conn;
+	
+	public PetDbUtil (DataSource dataSource) {
+		this.dataSource = dataSource;
+		
+		try {
+			conn = dataSource.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	private void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
